c14959660a40ea5eca3ca741544177b6e4a482e7
Himadri Das <codingmaster40@gmail.com>
2023-08-03 15:03:01 +0530

    tabulation and space optimization of BuySell 2

--- DP/Dp_on_Stocks/BestTimeToBuy&Sell_2.cpp
+++ DP/Dp_on_Stocks/BestTimeToBuy&Sell_2.cpp
@@ -28,9 +28,76 @@ long f(int ind, int buy, int &n, long *values, vector<vector<long>> &dp) {
     }
     return dp[ind][buy] = profit;
 }
+
+long spaceOptimization(long *values, int n){
+
+    //2 size 2 vector space optimization : 
+    vector<long> ahead(2, 0), cur(2, 0);
+    //bc
+    ahead[0] = ahead[1] = 0;
+
+    //nested loops : in tab : bottoms up : from bc -> to main problem
+    for(int ind = n-1; ind >= 0; ind--) {
+        for(int buy = 0; buy <= 1; buy++) {
+            long profit = 0;
+            if(buy) {
+                profit = max(-values[ind] + ahead[0], ahead[1]);
+            }
+            else {
+                profit = max(values[ind] + ahead[1], ahead[0]);
+            }
+            cur[buy] = profit;
+        }
+        ahead = cur;
+    }
+    //final ans is in dp[0][1] : at ind 0 we need to buy : give me the max profit that can be earned
+    return ahead[1];
+
+
+    /*****************SAME AS ABOVE**************************************/
+
+
+    //4 pointer space optimization 
+    long aheadNotBuy = 0, aheadBuy = 0;
+    long curNotBuy, curBuy;
+
+    for(int ind = n-1; ind >= 0; ind--) {
+        //for buy
+        curBuy = max(-values[ind] + aheadNotBuy, aheadBuy);
+
+        //for notbuy i.e sell
+        curNotBuy = max(values[ind] + aheadBuy, aheadNotBuy);
+
+        //save cur in ahead for nxt itern
+        aheadBuy = curBuy, aheadNotBuy = curNotBuy;
+    }
+    return aheadBuy;
+}
+
 long getMaximumProfit(long *values, int n)
 {
     //Write your code here
-    vector<vector<long>> dp(n, vector<long> (2, -1));
-    return f(0, 1, n, values, dp);//1 bcz initially buy a stock
+    // vector<vector<long>> dp(n, vector<long> (2, -1));
+    // return f(0, 1, n, values, dp);//1 bcz initially buy a stock
+
+    //tabulation : 
+    vector<vector<long>> dp(n+1, vector<long> (2, 0));
+    //bc
+    dp[n][0] = dp[n][1] = 0;
+
+    //nested loops : in tab : bottoms up : from bc -> to main problem
+    for(int ind = n-1; ind >= 0; ind--) {
+        for(int buy = 0; buy <= 1; buy++) {
+            long profit = 0;
+            if(buy) {
+                profit = max(-values[ind] + dp[ind+1][0], dp[ind+1][1]);
+            }
+            else {
+                profit = max(values[ind] + dp[ind+1][1], dp[ind+1][0]);
+            }
+            dp[ind][buy] = profit;
+        }
+    }
+    //final ans is in dp[0][1] : at ind 0 we need to buy : give me the max profit that can be earned
+    return dp[0][1];
 }
